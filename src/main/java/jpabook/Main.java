5e624b2b9deb3b2b545f86f946d84b5a0258bb99
DaeHyeonHong <fholng@outlook.com>
2022-09-05 22:05:22 +0900

    요구사항 분석 및 기본 매핑_2

--- src/main/java/jpabook/Main.java
+++ src/main/java/jpabook/Main.java
@@ -1,9 +1,24 @@
 package jpabook;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args) {
-        Persistence.createEntityManagerFactory()
+        final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+        final EntityManager entityManager = entityManagerFactory.createEntityManager();
+        final EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        try {
+            transaction.commit();
+        } catch (final Exception exception) {
+            transaction.rollback();
+        } finally {
+            entityManager.close();
+            entityManagerFactory.close();
+        }
     }
 }
